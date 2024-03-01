1eb5a5ad4fc73b9625959a86b4d213e22ddc77a3
Gomdeuki <122350642+Gomdeuki@users.noreply.github.com>
2024-03-01 15:35:00 +0700

    Add files via upload

---
+++ python_microservices/deleteuser/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, redirect, jsonify
+import mysql.connector as mysql
+
+conn = mysql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '1234567',
+    port =  3306,
+    database = 'my_memo'
+)
+
+app = Flask(__name__)
+@app.route('/delete/<email>', methods=['DELETE'])
+def delete_user(email):
+    cur = conn.reconnect()
+    cur = conn.cursor()
+    sql = "DELETE FROM memo WHERE email= %s"
+    data = (email,)
+    cur.execute(sql, data)
+    conn.commit()
+    conn.close()
+    return redirect('http://localhost:5002/getuser')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002, debug=True)
